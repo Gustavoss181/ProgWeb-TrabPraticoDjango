bad876e8e48443ea799536b374c7ba5c0f65f0f7
Gustavo Silva <gustavoss181@gmail.com>
2024-12-07 19:33:30 -0300

    Modificacoes dos modelos de produtos

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Guitarra, Categoria
+from .models import Guitarra, Violao, Baixo, Produto
 
 # Register your models here.
-admin.site.register(Categoria)
+admin.site.register(Produto)
 admin.site.register(Guitarra)
+admin.site.register(Violao)
+admin.site.register(Baixo)
--- produtos/models.py
+++ produtos/models.py
@@ -1,21 +1,67 @@
 from django.db import models
 
 # Create your models here.
-class Categoria(models.Model):
-    nome = models.CharField(max_length=50, unique=True)
-    descricao = models.TextField()
-
-    def __str__(self):
-        return self.nome
-
-class Guitarra(models.Model):
-    nome = models.CharField(max_length=100)
-    marca = models.CharField(max_length=50)
+class Produto(models.Model):
+    unique_id = models.AutoField(primary_key=True)
+    codigo = models.CharField(max_length=100, unique=True)  # Garantindo unicidade
+    quantidade = models.PositiveIntegerField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    estoque = models.IntegerField()
-    descricao = models.TextField()
-    # imagem = models.ImageField(upload_to='guitarras/')
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='guitarras', default=1)
 
     def __str__(self):
-        return self.nome
+        return self.codigo
+
+# Guitarra
+class Guitarra(models.Model):
+    unique_id = models.AutoField(primary_key=True)
+    produto = models.OneToOneField(Produto, on_delete=models.CASCADE, related_name="guitarra")
+    modelo = models.CharField(max_length=100)
+    serie = models.CharField(max_length=100, unique=True)  # Garantindo unicidade
+    numero_trastes = models.PositiveIntegerField()
+    numero_cordas = models.PositiveIntegerField()
+    captadores = models.CharField(max_length=100)
+    cor = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.modelo
+
+# Violão
+class Violao(models.Model):
+    TIPOS_CORDA = [
+        ('nylon', 'Nylon'),
+        ('aco', 'Aço'),
+    ]
+
+    TIPOS_CORPO = [
+        ('classico', 'Clássico'),
+        ('folk', 'Folk'),
+        ('jumbo', 'Jumbo'),
+        ('flat', 'Flat'),
+        ('outros', 'Outros'),
+    ]
+
+    unique_id = models.AutoField(primary_key=True)
+    produto = models.OneToOneField(Produto, on_delete=models.CASCADE, related_name="violao")
+    modelo = models.CharField(max_length=100)
+    serie = models.CharField(max_length=100, unique=True)  # Garantindo unicidade
+    numero_trastes = models.PositiveIntegerField()
+    numero_cordas = models.PositiveIntegerField()
+    tipo_corpo = models.CharField(max_length=10, choices=TIPOS_CORPO)
+    tipo_corda = models.CharField(max_length=10, choices=TIPOS_CORDA)
+    cor = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.modelo
+
+# Baixo
+class Baixo(models.Model):
+    unique_id = models.AutoField(primary_key=True)
+    produto = models.OneToOneField(Produto, on_delete=models.CASCADE, related_name="baixo")
+    modelo = models.CharField(max_length=100)
+    serie = models.CharField(max_length=100, unique=True)  # Garantindo unicidade
+    numero_trastes = models.PositiveIntegerField()
+    numero_cordas = models.PositiveIntegerField()
+    captadores = models.CharField(max_length=100)
+    cor = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.modelo
