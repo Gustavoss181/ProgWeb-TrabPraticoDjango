d677bbd32dd1c54e391695a8a975f361cb8f5b70
Gustavo Silva <gustavoss181@gmail.com>
2024-12-07 23:08:20 -0300

    criacao do modelo pedidos e fix das urls

--- GuitarStore/settings.py
+++ GuitarStore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'produtos',
     'usuarios',
+    'pedidos',
 ]
 
 MIDDLEWARE = [
---
+++ pedidos/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from produtos.models import Produto
+from usuarios.models import Usuario
+
+# Create your models here.
+class Pedido(models.Model):
+    unique_id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="pedidos")
+    data_pedido = models.DateTimeField(auto_now_add=True)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"Pedido {self.unique_id} - Usuário {self.usuario.username}"
+
+    def calcular_valor_total(self):
+        total = sum(item.quantidade * item.preco_unitario for item in self.itens.all())
+        self.valor_total = total
+        self.save()
+
+# Item do Pedido (Tabela Associativa)
+class ItemPedido(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name="itens_pedido")
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name="itens")
+    quantidade = models.PositiveIntegerField()
+    preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        unique_together = ("produto", "pedido")  # Garante que o mesmo produto não apareça duas vezes no mesmo pedido
+
+    def __str__(self):
+        return f"Produto {self.produto.codigo} no Pedido {self.pedido.unique_id}"
