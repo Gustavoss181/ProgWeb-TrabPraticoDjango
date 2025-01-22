1c280da77f11893b053f1360bb8b4c3913d19f78
Gustavo Silva <gustavoss181@gmail.com>
2025-01-21 23:28:03 -0300

    tela produto_detalhe

--- produtos/urls.py
+++ produtos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<int:produto_id>/', views.produto_detalhe, name='produto_detalhe'),
+    path('adicionar-ao-carrinho/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from produtos.models import Produto
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the produtos index.")
+
+def produto_detalhe(request, produto_id):
+    produto = get_object_or_404(Produto, id=produto_id)
+    return render(request, 'produtos/produto_detalhe.html', {'produto': produto})
+
+def adicionar_ao_carrinho(request, produto_id):
+    if request.method == "POST":
+        quantidade = int(request.POST.get('quantidade', 1))
+        # Aqui você pode implementar a lógica de adicionar ao carrinho.
+        # Exemplo: salvar no banco ou na sessão
+        # Para simplificar, redirecionamos de volta à página do produto.
+        return HttpResponseRedirect(reverse('produto_detalhe', args=[produto_id]))
