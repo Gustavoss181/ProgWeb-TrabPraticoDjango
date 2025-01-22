d65796296627c30ad34aa4a8eb813d1ee46684f6
Gustavo Silva <gustavoss181@gmail.com>
2025-01-22 18:12:32 -0300

    add: busca

--- loja/views.py
+++ loja/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.db.models import Q
 from produtos.models import Produto
 
 # Create your views here.
 def home(request):
+    query = request.GET.get('q', '')
     produtos = Produto.objects.all()
-    return render(request, 'loja/home.html', {'produtos': produtos})
+
+    if query:
+        produtos = produtos.filter(
+            Q(produto_instrumento__instrumento_corda__modelo__icontains=query) |
+            Q(produto_instrumento__instrumento_corda__tipo_instrumento__icontains=query) |
+
+            Q(produto_instrumento__instrumento_sopro__tipo_instrumento__icontains=query) |
+
+            Q(produto_instrumento__instrumento_percussao__tipo_instrumento__icontains=query)
+        )
+    return render(request, 'loja/home.html', {'produtos': produtos, 'query': query})
