1459b9c33bfeea118b67dec982640b46e97a5803
Rafael Brunini Pereira <bruninirafael@gmail.com>
2025-01-26 16:34:46 -0300

    feat: login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.autenticar, name='autenticar'),
+    path('', views.home, name='home')
 ]
